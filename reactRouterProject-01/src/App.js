dfe433bdffa7ebe75a56cc4472fe51f70365401e
shannone <shannone426@gmail.com>
2022-12-27 13:16:14 -0500

    added products path

--- reactRouterProject-01/src/App.js
+++ reactRouterProject-01/src/App.js
@@ -1,11 +1,15 @@
 import { Route } from 'react-router-dom';
 import Welcome from './components/Welcome';
+import Products from './components/Products';
 
 function App() {
   return (
     <div>
       <Route path="/welcome">
         <Welcome/>
+        </Route>
+        <Route path="/product">
+          <Products/>
         </Route>
     </div>
   );
